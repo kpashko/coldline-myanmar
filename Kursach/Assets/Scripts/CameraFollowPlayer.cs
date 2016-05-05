4d21721ff48acf8ea532842634cc8ef8b68fba50
AlexeyKislukhin <Kislukhin.Lesha@gmail.com>
2016-05-06 00:12:57 +0300

    add scripts to move and rotate player

---
+++ Kursach/Assets/Scripts/CameraFollowPlayer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowPlayer : MonoBehaviour {
+	GameObject player;
+	bool followPlayer = true;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (followPlayer == true) {
+			canFollowPlayer ();
+		}
+	}
+
+	public void setFollowPlayer (bool val){
+		followPlayer = val;
+	}
+	void canFollowPlayer(){
+		Vector3 newPos = new Vector3 (player.transform.position.x, player.transform.position.y, this.transform.position.z);
+		this.transform.position = newPos;
+	}
+}
---
+++ Kursach/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public bool moving = false;
+	float speed = 5.0f;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		movement ();
+	}
+
+	void movement(){
+		if (Input.GetKey (KeyCode.W)) {
+			transform.Translate (Vector3.up * speed * Time.deltaTime, Space.World);
+			moving = true;
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			transform.Translate (Vector3.down * speed * Time.deltaTime, Space.World);
+			moving = true;
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Translate (Vector3.left * speed * Time.deltaTime, Space.World);
+			moving = true;
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Translate (Vector3.right * speed * Time.deltaTime, Space.World);
+			moving = true;
+		}
+	}
+}
